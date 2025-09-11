c483d0e10877587464489b8424e5eaa1bd5f4de1
marzia <marziasultanar01@gmail.com>
2025-09-11 11:23:01 +0600

    add middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI, Request
 import uvicorn
 from app.routes import router 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Reelty AI API",
     docs_url="/ai-api/v1",
+)
+
+# origins = [
+#     "http://localhost:5173",  # frontend
+#     "http://127.0.0.1:5173"
+# ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
